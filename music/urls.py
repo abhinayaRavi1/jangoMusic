1b457fc6264295465cf7a1a53cac9aa85d4803b6
Abhinaya <Abhinaya@gmail.com>
2018-10-11 15:12:52 -0400

    Added Favorite song sleect option and modified css files

--- music/urls.py
+++ music/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, re_path
 from . import views
 
+app_name= 'music'
 urlpatterns = [
         # /music/
         path('', views.index, name='index'),
@@ -8,4 +9,6 @@ urlpatterns = [
         # /music/id_no/
         re_path(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),
 
+        # /music/<album_id>/favorite/
+        re_path(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite'),
 ]
--- music/views.py
+++ music/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render
-
-# Create your views here.
-from django.http import  HttpResponse
-from .models import Album
-#from django.template import loader
-from django.shortcuts import render
+# Create your views here
+from .models import Album, Song
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 
 def index(request):
     all_albums = Album.objects.all()
-    #template = loader.get_template('music/index.html')
-    #return HttpResponse(template.render(context,request))
     return render(request, 'music/index.html',{'all_albums': all_albums})
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404("404 Object Not found! ")
+    album = get_object_or_404(Album,pk=album_id)
     return render(request, 'music/details.html', {'album': album})
+
+def favorite(request,album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/details.html', {
+            'album':album,
+            'error_message': "No song selected",
+        })
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request,'music/details.html', {'album':album})
